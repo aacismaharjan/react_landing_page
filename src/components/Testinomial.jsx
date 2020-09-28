98a3faa2fa109ac5b804a9cb254e82f38f3a1f51
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-09-28 13:09:09 +0545

    Made Testinomial Reusable component

--- src/components/Testinomial.jsx
+++ src/components/Testinomial.jsx
@@ -3,7 +3,70 @@ import Testi1 from '../img/testimonial/1.jpg'
 import Testi2 from '../img/testimonial/2.jpg'
 import Testi3 from '../img/testimonial/3.jpg'
 
+const TestinomialItem = ({ testi: { comment, avatar, name, post } }) => {
+  return (
+    <div class='testi-item'>
+      <div class='testi-comment'>
+        <p>
+          <i class='fas fa-quote-left'></i>
+          {comment}
+          <i class='fas fa-quote-right'></i>
+        </p>
+
+        <ul class='stars list-unstyled'>
+          <li>
+            <i class='fas fa-star'></i>
+          </li>
+          <li>
+            <i class='fas fa-star'></i>
+          </li>
+          <li>
+            <i class='fas fa-star'></i>
+          </li>
+          <li>
+            <i class='fas fa-star-half-alt'></i>
+          </li>
+          <li>
+            <i class='far fa-star'></i>
+          </li>
+        </ul>
+      </div>
+
+      <div class='client-info'>
+        <img src={avatar} alt='testinomial' />
+        <h5>{name}</h5>
+        <p>{post}</p>
+      </div>
+    </div>
+  )
+}
+
+export { TestinomialItem }
+
 export default function Testinomial() {
+  const testiItems = [
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      avatar: Testi1,
+      name: 'Soshiv Upreti',
+      post: 'CEO Webshala',
+    },
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      avatar: Testi2,
+      name: 'Pine Apple',
+      post: 'CEO Webshala',
+    },
+    {
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      avatar: Testi3,
+      name: 'Rakesh Singh',
+      post: 'CEO Webshala',
+    },
+  ]
   return (
     <section class='testinomial-section' id='testinomial'>
       <div class='container'>
@@ -18,119 +81,11 @@ export default function Testinomial() {
 
         <div class='row'>
           <div class='owl-carousel' id='testinomial-carousel'>
-            <div class='col-lg-12'>
-              <div class='testi-item'>
-                <div class='testi-comment'>
-                  <p>
-                    <i class='fas fa-quote-left'></i>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                    <i class='fas fa-quote-right'></i>
-                  </p>
-
-                  <ul class='stars list-unstyled'>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star-half-alt'></i>
-                    </li>
-                    <li>
-                      <i class='far fa-star'></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div class='client-info'>
-                  <img src={Testi1} alt='testinomial' />
-                  <h5>Soshiv Upreti</h5>
-                  <p>CEO Webshala</p>
-                </div>
+            {testiItems.map((testi, index) => (
+              <div class='col-lg-12' key={index}>
+                <TestinomialItem testi={testi} />
               </div>
-            </div>
-
-            <div class='col-lg-12'>
-              <div class='testi-item'>
-                <div class='testi-comment'>
-                  <p>
-                    <i class='fas fa-quote-left'></i>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                    <i class='fas fa-quote-right'></i>
-                  </p>
-
-                  <ul class='stars list-unstyled'>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star-half-alt'></i>
-                    </li>
-                    <li>
-                      <i class='far fa-star'></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div class='client-info'>
-                  <img src={Testi2} alt='testinomial' />
-                  <h5>Pine Apple</h5>
-                  <p>CEO Webshala</p>
-                </div>
-              </div>
-            </div>
-
-            <div class='col-lg-12'>
-              <div class='testi-item'>
-                <div class='testi-comment'>
-                  <p>
-                    <i class='fas fa-quote-left'></i>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                    <i class='fas fa-quote-right'></i>
-                  </p>
-
-                  <ul class='stars list-unstyled'>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star'></i>
-                    </li>
-                    <li>
-                      <i class='fas fa-star-half-alt'></i>
-                    </li>
-                    <li>
-                      <i class='far fa-star'></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div class='client-info'>
-                  <img src={Testi3} alt='testinomial' />
-                  <h5>Jitender Kumar</h5>
-                  <p>CEO Webshala</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
